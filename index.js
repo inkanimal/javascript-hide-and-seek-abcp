a11a350f00c7975105b73db65b19eff2bd2739a8
geoffrey woodcock <theatomproject@gmail.com>
2018-01-21 04:48:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector(`#nested, .target`)
 }
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll(`ul.ranked-list li`);
+  for (var i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
