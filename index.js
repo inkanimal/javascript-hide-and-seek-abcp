bac10770f540c3f52b84194e9db3c74c2bb142a1
geoffrey woodcock <theatomproject@gmail.com>
2018-01-21 05:01:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n){
   }
 }
 function deepestChild(){
-  var grandNode = document.querySelector(`#grand-node`);
+  var grandNode = document.querySelectorAll(`#grand-node`);
   var deepestNode = grandNode.children[0];
   for (var i = 0; !deepestNode.children[i]; i){
     deepestNode = deepestNode.children[0]
